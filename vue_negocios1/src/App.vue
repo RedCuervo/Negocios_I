c40b16d9ea2dddda41885795f9464a8294d38020
RedCuervo <sael_agua@hotmail.com>
2024-07-05 00:19:52 -0600

    vue

---
+++ vue_negocios1/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+import theHeader from '../src/components/theHeader.vue'
+import theFooter from '../src/components/theFooter.vue'
+</script>
+
+<template>
+<header>
+  <theHeader />
+</header>
+<main>
+  <RouterView />
+</main>
+<footer>
+  <theFooter />
+</footer>
+</template>
+
+<style scoped>
+main {
+  padding-top: 150px; /* Ajusta este valor según la altura de tu encabezado */
+}
+</style>
